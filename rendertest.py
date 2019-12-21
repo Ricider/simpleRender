dd116e6936872d77cc2a4399401f13a9415f448a
Ricider <54453420+Ricider@users.noreply.github.com>
2019-12-20 21:47:04 -0500

    Add files via upload

--- rendertest.py
+++ rendertest.py
@@ -25,11 +25,13 @@ class point:
             self.z*integer)        
         
 class polygon:
+    allpollys=[]
     def __init__(self,x,y,z,color):
         self.point1=x
         self.point2=y
         self.point3=z
         self.color=color
+        polygon.allpollys.append(self)
         
     def draw(self):
         temp=self.point1.subtract(camera)
@@ -41,32 +43,55 @@ class polygon:
         temp=self.point3.subtract(camera)
         proj3=camera.add(temp.integerMul(-(camera.z/temp.z)))
         
+        #turtle stuff
+        tt.fillcolor(self.color)        
         tt.setpos(proj1.x*scale,proj1.y*scale)
+        tt.begin_fill()
         tt.pendown()
         tt.setpos(proj2.x*scale,proj2.y*scale)
         tt.setpos(proj3.x*scale,proj3.y*scale)
         tt.setpos(proj1.x*scale,proj1.y*scale)
+        tt.end_fill()
         tt.penup()
-                
         
+    def drawframe():
+        for poly in polygon.allpollys:
+            poly.draw()
+                
+class cube:
+    def __init__(self,pos,size):
+        self.polylist=[]
+        
+        #edges
+        x1=pos
+        x2=point(pos.x,pos.y+size,pos.z)
+        x3=point(pos.x+size,pos.y,pos.z)
+        x4=point(pos.x+size,pos.y+size,pos.z)
+        x5=point(pos.x,pos.y+size,pos.z+size)
+        x6=point(pos.x+size,pos.y+size,pos.z+size)
+        x7=point(pos.x+size,pos.y,pos.z+size)
+        
+        #faces
+        a1=polygon(x1,x2,x3,"Red")
+        a2=polygon(x4,x2,x3,"Red")
+        b1=polygon(x4,x2,x5,"Blue")
+        b2=polygon(x5,x4,x6,"Blue")
+        c1=polygon(x4,x7,x3,"Green")
+        c2=polygon(x4,x6,x7,"Green")                   
+
 #prep
 tt=turtle.Turtle()
-tt.color("red")
+tt.color("Black")
 tt.hideturtle()
 tt.penup()
-tt.speed(0)
+tt.speed(4)
 
 #perspective elements
-camera=point(0,0,-5)
+camera=point(0,0,-20)
 projPlaneZ=0
-scale=20
+scale=60
 
-testpoly=polygon(
-point(2,0,1),
-point(0,0,1),
-point(0,2,1),
-"Red")
-
-testpoly.draw()
+cube(point(-5,-5,5),1)
+polygon.drawframe()
 
 turtle.exitonclick()
